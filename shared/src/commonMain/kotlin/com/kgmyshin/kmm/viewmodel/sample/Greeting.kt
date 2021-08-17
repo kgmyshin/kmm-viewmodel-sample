b6cc1ad6953e63e108804d3f207615136f0b4056
kgmyshin <kgmyshin82@gmail.com>
2021-08-17 23:20:06 +0900

    不要ファイル削除

--- shared/src/commonMain/kotlin/com/kgmyshin/kmm/viewmodel/sample/Greeting.kt
+++
@@ -1,7 +0,0 @@
-package com.kgmyshin.kmm.viewmodel.sample
-
-class Greeting {
-    fun greeting(): String {
-        return "Hello, ${Platform().platform}!"
-    }
-}
--- shared/src/commonMain/kotlin/com/kgmyshin/kmm/viewmodel/sample/Platform.kt
+++
@@ -1,5 +0,0 @@
-package com.kgmyshin.kmm.viewmodel.sample
-
-expect class Platform() {
-    val platform: String
-}
